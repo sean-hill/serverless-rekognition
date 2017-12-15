c4eb4cde6644729c98fed166aee42c0f45c9af6a
Sean Hill <sean.daryl.hill@gmail.com>
2017-12-15 08:41:17 -0700

    Demo rekognition example

---
+++ README.md
@@ -0,0 +1,17 @@
+# Serverless Rekognition Example
+
+This repository provides a code sample of how to accept an image via `POST`
+request and process that image to see if any celebrities are contained within
+it.
+
+## Known Issues
+
+The `multer` library does not seem to work with the `serverless-http` library.
+When the express app is tested by itself by uncommenting the `app.listen` line
+and running `node index.js` the image uploads work perfectly and Rekognition can
+process the image as it should. However, when the express app is wrapped with
+the `serverless-http` library, the image uploads are corrupted and are not
+parsed as an image.
+
+This was tested using `serverless-offline` as well as in AWS with Postman as
+seen below.
---
+++ index.js
@@ -0,0 +1,45 @@
+const serverless = require('serverless-http')
+const express = require('express')
+const multer = require('multer')
+const fs = require('fs')
+const AWS = require('aws-sdk')
+const app = express()
+const storage = multer.memoryStorage()
+const upload = multer({ storage })
+
+/*
+ *  Accepts an image and sends the image to Rekognition to detect any celebrities
+ */
+app.post('/lookup-celebrities', upload.single('image'), (req, res) => {
+  const rekognition = new AWS.Rekognition()
+  const image = req.file
+
+  if (!image) {
+    return res.status(400).json({ message: 'Please provide an image.' })
+  }
+
+  const params = {
+    Image: {
+      Bytes: image.buffer
+    }
+  }
+
+  rekognition.recognizeCelebrities(params, (err, response) => {
+    if (err) {
+      return res.status(400).json({ message: err.message })
+    }
+
+    const celebrities = response.CelebrityFaces.map(celebrity => ({
+      name: celebrity.Name
+    }))
+
+    res.status(200).json(celebrities)
+  })
+})
+
+/*
+ *  Uncomment to test locally without serverless-http, it works
+ */
+// app.listen(3000, () => console.log('Listening on port 3000'))
+
+module.exports.handler = serverless(app)
